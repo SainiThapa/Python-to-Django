7de3684c46aadf0da6a142e68666d8f6a5c18031
Saini Thapa <magarprisu4747@gmail.com>
2023-03-09 15:09:42 +0545

    adding to empty Dictionary

---
+++ Week 1/Day3/04practice.py
@@ -0,0 +1,10 @@
+A={}
+a=input("Enter your favourite language Raj: ")
+b=input("Enter your favourite language Ramesh: ")
+c=input("Enter your favourite language Roshan: ")
+d=input("Enter your favourite language Rajesh: ")
+A['Raj']=a
+A['Ramesh']=b
+A['Roshan']=c
+A['Rajesh']=d
+print(A)

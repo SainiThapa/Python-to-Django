1e4c6d4942be8927ed3cd9a5f6cf83c614ccb3ee
Saini Thapa <magarprisu4747@gmail.com>
2023-03-10 12:45:13 +0545

    If/elif/else conditions

---
+++ Week 1/Day4/01condition.py
@@ -0,0 +1,8 @@
+a=34
+b=int(input("Enter a number: "))
+if(a>b):
+    print("The number you entered is less than 34")
+elif(a==b):
+    print("You entered 34")
+else:
+    print("The number you entered is greater than 34")

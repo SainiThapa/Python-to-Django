8fcc7577b81d8f0df66b04588447c3b6c54a26cd
Saini Thapa <magarprisu4747@gmail.com>
2023-03-10 15:05:35 +0545

    While loop

---
+++ Week 1/Day4/Loops/01loops.py
@@ -0,0 +1,13 @@
+#WHILE LOOP
+a=['Cat','Rat','Bat','Sat','Hat']
+print(a)
+b=len(a)
+c='Sadistic'
+i=0
+j=0
+while i<b:
+    print(a[i])
+    i=i+1
+while j<len(c):
+    print(c[j],end=" ")
+    j=j+1

4e4906853f5847300820d4ae78cfaabfdebb9a61
Saini Thapa <magarprisu4747@gmail.com>
2023-03-10 15:08:32 +0545

    For-IN example

---
+++ Week 1/Day4/Loops/practice02.py
@@ -0,0 +1,9 @@
+#WAP to greet all the person names in the lists and starting with S
+# List=["Sam","Harry","Sejal","Ram","Vikram"]
+List=["Sam","Harry","Sejal","Ram","Vikram"]
+
+for name in List:
+    if(name.startswith("S")):
+        print("Hello",name)
+    # else:
+    #     print(name)

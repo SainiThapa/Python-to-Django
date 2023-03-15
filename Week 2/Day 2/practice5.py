ae2e8e7c58b032f8fbacb4d09e33f965a2f11c54
Saini Thapa <magarprisu4747@gmail.com>
2023-03-15 15:47:12 +0545

    Mining log file to search 'python'

---
+++ Week 2/Day 2/practice5.py
@@ -0,0 +1,16 @@
+#mine a sample log file and find out whether it contains python or not
+# also specify in which line is the word 'python' present
+with open("log.txt") as f:
+    con=f.read()
+if 'python' in con.lower():
+    print("Yes python is present on the file")
+else:
+    print("Not present")
+line=True
+i=1
+with open("log.txt") as g:
+    while(line):
+        line=g.readline()
+        if 'python' in line.lower():
+            print("python is present on the line",str(i));
+        i+=1

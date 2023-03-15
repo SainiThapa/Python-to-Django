43c0abf9661f6eabd9f0d72e9d987694326dc077
Saini Thapa <magarprisu4747@gmail.com>
2023-03-15 14:41:33 +0545

    search in file

---
+++ Week 2/Day 2/practice1.py
@@ -0,0 +1,8 @@
+#WAP to open "article.txt" and check if 'Euler' is present in the file or not
+f=open("article.txt",'r')
+a=f.read()
+b=input("Enter a keyword to search in the file: ")
+if b in a:
+    print("Euler is present in the article")
+else:
+    print("Euler is not present in the article")

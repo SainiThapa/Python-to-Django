f49df50c1fcc2fc244667ec30e41f0f7d6d5a239
Saini Thapa <magarprisu4747@gmail.com>
2023-03-15 14:31:17 +0545

    append mode

--- Week 2/Day 2/04file.py
+++ Week 2/Day 2/04file.py
@@ -1,1 +1,11 @@
 # Append mode
+f=open("python.txt",'a')
+a=input("Enter a string: ")
+f.write(a)
+b=input("Enter a string: ")
+f.write(b)
+f.close()
+g=open("python.txt",'r')
+c=g.read()
+print(c)
+g.close

5a3158b558fe2ece07fcb22d57e0e2b5ba5d9f67
Saini Thapa <magarprisu4747@gmail.com>
2023-03-16 22:25:21 +0545

    Introduction to class and objects

---
+++ Week 2/Day 3/01oops.py
@@ -0,0 +1,17 @@
+# Intro to class and objects
+class student:
+    def name(self):
+        return self.a+self.b
+name1=student()
+name1.a="Saini"
+name1.b="Thapa"
+s=name1.name()
+print(s)
+
+
+'''
+EmployeeName --> PascalCase 
+Classes are written in PascalCase
+
+isInt, isFloat --> camelCase
+'''

f8e1b14b09f0d8ebed4136a08cfdee59b0eca60d
Saini Thapa <magarprisu4747@gmail.com>
2023-03-17 00:05:44 +0545

    Square and cube of a number using class

--- Week 2/Day 3/practice02.py
+++ Week 2/Day 3/practice02.py
@@ -1,9 +1,11 @@
 import math
 class Calculator:
     def square(self):
-        print( pow(self.num,2))
+        # print( pow(self.num,2))
+        print(self.num**2) #This can also be written if you dont want to import the math library
     def squareroot(self):
-        print(pow(self.num1,0.5))
+        a=pow(self.num1,0.5)
+        print(round(a,3))
     def cube(self):
         print(pow(self.num2,3))
 s1=Calculator()
@@ -11,7 +13,7 @@ s1.num=int(input("Enter a number to find the square: "))
 s1.square()
 s1.num1=int(input("Enter a number to find the square root: "))
 s1.squareroot()
-s1.num2=int(input("Enter a number to find the cube root : "))
+s1.num2=int(input("Enter a number to find the cube : "))
 s1.cube()
 
     
